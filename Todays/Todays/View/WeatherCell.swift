dff1317582717a10ab3f140ffb41d263bc61354e
Yehia Arafa <yehiaarafa11@gmail.com>
2018-03-16 08:10:18 -0600

    fixed weather layout issues

--- Todays/Todays/View/WeatherCell.swift
+++ Todays/Todays/View/WeatherCell.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class WeatherCell: UITableViewCell {
-
-
+    
+    
     @IBOutlet weak var city: UILabel!
     @IBOutlet weak var date_number: UILabel!
     @IBOutlet weak var date_day: UILabel!
@@ -26,7 +26,7 @@ class WeatherCell: UITableViewCell {
         // Initialization code
         weather.font = UIFont(name: "DINCondensed-Bold", size: 42)!
     }
-
+    
     
     override var frame: CGRect {
         get {
@@ -42,10 +42,10 @@ class WeatherCell: UITableViewCell {
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
     override func prepareForReuse() {
         super.prepareForReuse()
         downloadTask?.cancel()
@@ -55,9 +55,9 @@ class WeatherCell: UITableViewCell {
     func setLabels(for searchResult: WeatherCityResult, currentCity: Int, celsius: Bool){
         switch currentCity {
         case 0:
-            city.text = "Las Cruces"
+            city.text = " Las Cruces"
         case 1:
-            city.text = "El Paso"
+            city.text = "    El Paso"
         case 2:
             city.text = "Albuquerque"
         default:
@@ -65,16 +65,16 @@ class WeatherCell: UITableViewCell {
         }
         weatherCondition.text = searchResult.weatherCondition
         if(celsius){
-            weather.text = searchResult.temperature_c
-             feels.text = searchResult.feels_c
+            weather.text = " "+searchResult.temperature_c
+            feels.text = searchResult.feels_c
             format.image = UIImage(named: "celsius")
         }
         else{
-            weather.text = searchResult.temperature_f
+            weather.text = "  "+searchResult.temperature_f
             feels.text = searchResult.feels_f
             format.image = UIImage(named: "fahrenheit")
         }
-      
+        
         let date = Date()
         let calendar = Calendar.current
         
@@ -84,7 +84,7 @@ class WeatherCell: UITableViewCell {
         let stringMonth = mapMonth(month: String(month))
         date_day.text = stringMonth
     }
- 
+    
     func mapMonth(month: String)->String{
         switch(month){
         case "1":
@@ -119,3 +119,4 @@ class WeatherCell: UITableViewCell {
     
     
 }
+
